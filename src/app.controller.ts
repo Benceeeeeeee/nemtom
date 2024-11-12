0fb53dd125e8ce2851fed2642f8da10039f8b20d
Benceeeeeeee <144451434+Benceeeeeeee@users.noreply.github.com>
2024-11-12 14:15:03 +0100

    kereses

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { log } from 'console';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
@@ -11,5 +12,30 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  sutik = [
+    'Palacsinta',
+    'Tiramisu',
+    'Krémes',
+  ]
+  @Get('sutik')
+  sutemenyekListazas() {
+    return this.sutik;
+  }
+
+  @Get('sutik/:sutiid')
+  sutemenyIdAlapjan(@Param('sutiid') id: string) {
+    const idSzam = parseInt(id);
+
+    return this.sutik[idSzam];
+  }
+
+  @Get('sutiKereses')
+  sutemenyKereses(@Query('kereses') kereses?: string){
+    if(!kereses){
+      return this.sutik
+    }
+    return this.sutik.filter(suti => suti.toLocaleLowerCase().includes(kereses.toLocaleLowerCase()))
   }
 }
